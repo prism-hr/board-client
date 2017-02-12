0d9c639edf59810ba87315b4ab130c8c11bc53ea
alastairknowles <alastair@prism.hr>
2017-02-12 13:46:04 +0000

    Started adding the support for authentication.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { StormpathModule } from 'angular-stormpath';
 
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StormpathModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/authentication/authentication.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Stormpath, Account } from 'angular-stormpath';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-authentication',
+  templateUrl: './authentication.component.html',
+  styleUrls: ['./authentication.component.css']
+})
+export class AuthenticationComponent implements OnInit {
+  private user$: Observable<Account | boolean>;
+  private loggedIn$: Observable<boolean>;
+  private login: boolean;
+  private register: boolean;
+
+  constructor(public stormpath: Stormpath) {
+  }
+
+  ngOnInit() {
+    this.user$ = this.stormpath.user$;
+    this.loggedIn$ = this.user$.map(user => !!user);
+    this.login = true;
+    this.register = false;
+  }
+
+  showLogin() {
+    this.register = false;
+    this.login = true;
+  }
+
+  showRegister() {
+    this.login = false;
+    this.register = true;
+  }
+
+  logout() {
+    this.stormpath.logout();
+  }
+
+}
