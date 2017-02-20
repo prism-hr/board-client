025b7928465dc7210a5e1546d2bee1aa670041e4
fibinger <kuba@fibinger.pl>
2017-02-20 11:26:28 +0100

    More authentication aspects added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,35 +5,45 @@ import {HttpModule} from '@angular/http';
 import {StormpathModule, StormpathConfiguration} from 'angular-stormpath';
 import {AppComponent} from './app.component';
 import {AuthenticationDialog} from './authentication/authentication.dialog';
-import {WelcomeComponent} from './welcome/welcome.component';
+import {HomeComponent} from './home/home.component';
 import {NotFoundComponent} from './not-found.component';
 import {RouterModule} from '@angular/router';
 import {MaterialModule} from '@angular/material';
 import {stormpathConfig} from './stormpath.config';
 import {HeaderComponent} from './header/header.component';
 import {ActivitiesComponent} from './activities/activities.component';
+import {AuthedComponent} from './authed/authed.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {AuthGuard} from './authed/auth-guard.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     AuthenticationDialog,
     NotFoundComponent,
+    HomeComponent,
+    AuthedComponent,
     WelcomeComponent,
-    HeaderComponent,
     ActivitiesComponent
   ],
   imports: [
     RouterModule.forRoot([
-      {path: '', component: WelcomeComponent},
+      {path: '', component: HomeComponent},
+      {
+        path: '', component: AuthedComponent, canActivate: [AuthGuard], children: [
+        {path: 'welcome', component: WelcomeComponent}
+      ]
+      },
       {path: '**', component: NotFoundComponent}
     ]),
-    MaterialModule.forRoot(),
+    MaterialModule,
     BrowserModule,
     FormsModule,
     HttpModule,
     StormpathModule
   ],
-  providers: [{provide: StormpathConfiguration, useFactory: stormpathConfig}],
+  providers: [{provide: StormpathConfiguration, useFactory: stormpathConfig}, AuthGuard],
   entryComponents: [AuthenticationDialog],
   bootstrap: [AppComponent]
 })
