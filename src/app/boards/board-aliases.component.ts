2a848187a1ad4957ce2d0576353ffe4bde655cc6
Juan Mingo <juan.mingo@itv.com>
2017-04-01 17:41:52 +0200

    new board update

--- src/app/boards/board-aliases.component.ts
+++ src/app/boards/board-aliases.component.ts
@@ -5,23 +5,26 @@ import {FormGroupName, NG_VALUE_ACCESSOR} from '@angular/forms';
 @Component({
   selector: 'board-aliases',
   template: `
-    <div [formGroup]="formGroupName.control">
-      <md-input-container class="md-block">
-        <input mdInput name="applicationUrl" value="{{applicationUrl}}" disabled>
-      </md-input-container>
-      /
-      <md-input-container class="md-block">
-        <input mdInput formControlName="departmentHandle" placeholder="Department alias" required>
-      </md-input-container>
-      <control-messages [control]="formGroupName.control.controls.departmentHandle"></control-messages>
-
-      <span *ngIf="!hideBoard">
-        /
+    <div [formGroup]="formGroupName.control" class="grid url-builder">
+      <div class="grid__item one-third">
+        <md-input-container class="md-block">
+          <input mdInput name="applicationUrl" value="{{applicationUrl}}" disabled>
+        </md-input-container>
+        <span class="separator-right">/</span>
+      </div>
+      <div class="grid__item one-third">
+        <md-input-container class="md-block">
+          <input mdInput formControlName="departmentHandle" placeholder="Department alias" required>
+        </md-input-container>
+        <control-messages [control]="formGroupName.control.controls.departmentHandle"></control-messages>
+      </div>
+      <div class="grid__item one-third" *ngIf="!hideBoard">
+        <span class="separator-left">/</span>
         <md-input-container class="md-block">
           <input mdInput formControlName="boardHandle" placeholder="Board alias" required>
         </md-input-container>
         <control-messages [control]="formGroupName.control.controls.boardHandle"></control-messages>
-      </span>
+      </div>
     </div>
     `,
   styleUrls: [],
