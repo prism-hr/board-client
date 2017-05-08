1eeb9e97d85b2cd6851c96d458e0b3ad2f48475b
fibinger <kuba@fibinger.pl>
2017-05-08 22:06:22 +0200

    Handle control implemented accordingly to request

--- src/app/controls/resource-handle.component.ts
+++ src/app/controls/resource-handle.component.ts
@@ -1,18 +1,12 @@
-import {Component, forwardRef, Input, OnInit} from '@angular/core';
-import {DefinitionsService} from '../services/definitions.service';
-import {ControlValueAccessor, FormGroupName, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {Component, forwardRef, Input} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'b-resource-handle',
   template: `
     <span>{{urlPrefix}}</span>
-    <span *ngIf="!editing" (click)="edit()"><span>{{handle}}</span>
-    </span>
-    <span *ngIf="editing">
-      <input pInputText [(ngModel)]="editedHandle" required>
-      <button pButton type="button" class="ui-button-success ui-button-small" (click)="ok()" icon="fa-check"></button>
-      <button pButton type="button" class="ui-button-warning ui-button-small" (click)="cancel()" icon="fa-close"></button>
-    </span>
+    <input pInputText [(ngModel)]="handle" (change)="handleChanged($event)" required>
+    <button pButton type="button" (click)="copyToClipboard()" icon="fa-docs"></button>
   `,
   styleUrls: ['xeditable.scss'],
   providers: [
@@ -24,25 +18,16 @@ export class ResourceHandleComponent implements ControlValueAccessor {
 
   propagateChange: any;
   handle: string;
-  editedHandle: string;
-  editing: boolean;
 
   constructor() {
   }
 
-  edit() {
-    this.editing = true;
-    this.editedHandle = this.handle;
-  }
-
-  cancel() {
-    this.editing = false;
-  }
-
-  ok() {
-    this.handle = this.editedHandle;
-    this.editing = false;
+  handleChanged() {
     this.propagateChange(this.handle);
+  }
+
+  copyToClipboard() {
+
   }
 
   writeValue(handle: string): void {
