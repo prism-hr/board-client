1adf07e222636745c2ebd14738f78a5f6b4e11b1
fibinger <kuba@fibinger.pl>
2017-05-09 13:29:46 +0200

    Support for xeditable description, handling click outside

--- src/app/controls/xeditable-input.component.ts
+++ src/app/controls/xeditable-input.component.ts
@@ -1,33 +1,41 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, HostListener, Input} from '@angular/core';
 import {ResourceService} from '../services/resource.service';
 import {Response} from '@angular/http';
 
 @Component({
   selector: 'b-xeditable-input',
   template: `
-    <span *ngIf="!editing" [ngClass]="{editable: canEdit}" (click)="edit()"><span>{{resource[propertyName]}}</span>
+    <span [hidden]="editing" [ngClass]="{editable: canEdit}" (click)="edit()"><span>{{resource[propertyName]}}</span>
     </span>
     <span *ngIf="editing">
-      <input pInputText [(ngModel)]="editedName" required>
+      <input *ngIf="fieldType === 'input'" pInputText [(ngModel)]="editedName" required>
+      <textarea *ngIf="fieldType === 'textarea'" pInputTextarea [(ngModel)]="editedName" required></textarea>
       <button pButton type="button" class="ui-button-success ui-button-small" (click)="ok()" icon="fa-check"></button>
       <button pButton type="button" class="ui-button-warning ui-button-small" (click)="cancel()" icon="fa-close"></button>
     </span>
   `,
   styleUrls: ['xeditable.scss']
 })
-export class XeditableInputComponent implements OnInit {
+export class XeditableInputComponent {
 
   @Input() canEdit: boolean;
   @Input() resource: any;
   @Input() propertyName: string;
+  @Input() fieldType: string;
 
   editedName: string;
   editing: boolean;
 
-  constructor(private resourceService: ResourceService) {
+  constructor(private resourceService: ResourceService, private eRef: ElementRef) {
   }
 
-  ngOnInit(): void {
+  @HostListener('document:click', ['$event'])
+  documentClick(event) {
+    if (!this.eRef.nativeElement.contains(event.target)) { // clicked outside
+      if (this.editing) {
+        this.cancel();
+      }
+    }
   }
 
   edit() {
@@ -45,7 +53,7 @@ export class XeditableInputComponent implements OnInit {
   ok() {
     this.resourceService.patchBoard(this.resource.id, {[this.propertyName]: this.editedName})
       .subscribe(board => {
-        this.resource[this.propertyName] = board.name;
+        this.resource[this.propertyName] = board[this.propertyName];
         this.editing = false;
       }, (error: Response) => {
         if (error.status === 422) {
