6c3c287f281eadbd466acdc8b839671fe058f392
Juan Mingo <juan.mingo@itv.com>
2017-05-12 14:14:03 +0200

    some for ui changes

--- src/app/controls/resource-handle.component.ts
+++ src/app/controls/resource-handle.component.ts
@@ -5,14 +5,14 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
   selector: 'b-resource-handle',
   template: `
     <div class="grid">
-      <div class="grid__item one-third">
-        {{urlPrefix}}
+      <div class="grid__item one-third host-url">
+        <span>{{urlPrefix}}</span>
       </div>
       <div class="grid__item one-third">
         <input pInputText [(ngModel)]="handle" (change)="handleChanged($event)" required>
       </div>
       <div class="grid__item one-third">
-        <button pButton type="button" (click)="copyToClipboard()" icon="fa-docs"></button>
+        <button pButton type="button" (click)="copyToClipboard()" class="ui-button-success" icon="fa-docs"></button>
       </div>
     </div>
   `,
