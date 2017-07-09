b228e3f65b4938a9ec8def26f6f8c54af2ef2607
fibinger <kuba@fibinger.pl>
2017-07-09 18:52:40 +0200

    Dep versions updated, small way of post changes display

--- src/app/rxjs-extensions.ts
+++ src/app/rxjs-extensions.ts
@@ -1,4 +1,6 @@
+import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/observable/fromPromise';
