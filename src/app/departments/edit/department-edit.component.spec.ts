6206f970c48b3c40ef2b965e6103fa5dfac05813
fibinger <kuba@fibinger.pl>
2017-08-22 14:14:31 +0200

    Trying to fix cloudinary refresh problem, some test fixes

--- src/app/departments/edit/department-edit.component.spec.ts
+++ src/app/departments/edit/department-edit.component.spec.ts
@@ -1,28 +1,28 @@
-import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
-import {DepartmentViewComponent} from './department-view.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {ControlMessagesComponent} from '../../validation/control-messages.component';
-import {ButtonModule, ChipsModule} from 'primeng/primeng';
-import {BoardAliasesComponent} from '../../controls/board-aliases.component';
-import {FileUploadComponent} from '../../general/file-upload.component';
-import {CloudinaryModule} from '@cloudinary/angular';
-import {FileUploadModule} from 'ng2-file-upload';
-import {ActivatedRoute, Router} from '@angular/router';
-import {ResourceService} from '../../services/resource.service';
-import {MdSnackBarModule} from '@angular/material';
-import {DefinitionsService} from '../../services/definitions.service';
-import * as Cloudinary from 'cloudinary-core';
-import {RouterTestingModule} from '@angular/router/testing';
-import {FakeDefinitionsService} from '../../testing/fake-definitions.service';
-import {click, newEvent} from '../../testing/index';
-import {By} from '@angular/platform-browser';
-import {ActivatedRouteStub, RouterStub} from '../../testing/router-stubs';
-import {FakeResourceService} from '../../testing/fake-resource.service';
 import {DebugElement} from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MdSnackBarModule} from '@angular/material';
+import {By} from '@angular/platform-browser';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {CloudinaryModule} from '@cloudinary/angular-4.x';
+import * as Cloudinary from 'cloudinary-core';
+import {ButtonModule, ChipsModule, FileUploadModule} from 'primeng/primeng';
+import {ResourceHandleComponent} from '../../controls/resource-handle.component';
+import {FileUploadComponent} from '../../general/file-upload.component';
+import {DefinitionsService} from '../../services/definitions.service';
+import {ResourceService} from '../../services/resource.service';
+import {FakeDefinitionsService} from '../../testing/fake-definitions.service';
+import {FakeResourceService} from '../../testing/fake-resource.service';
+import {click, newEvent} from '../../testing/index';
+import {ActivatedRouteStub, RouterStub} from '../../testing/router-stubs';
+import {ControlMessagesComponent} from '../../validation/control-messages.component';
+import {DepartmentEditComponent} from './department-edit.component';
+import '../../rxjs-extensions';
 
 describe('DepartmentView', () => {
 
-  let fixture: ComponentFixture<DepartmentViewComponent>;
+  let fixture: ComponentFixture<DepartmentEditComponent>;
 
   class Page {
     submitSpy: jasmine.Spy;
@@ -50,7 +50,7 @@ describe('DepartmentView', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        DepartmentViewComponent, ControlMessagesComponent, BoardAliasesComponent, FileUploadComponent
+        DepartmentEditComponent, ControlMessagesComponent, ResourceHandleComponent, FileUploadComponent
       ],
       imports: [ReactiveFormsModule, RouterTestingModule, MdSnackBarModule, ButtonModule, ChipsModule,
         CloudinaryModule.forRoot(Cloudinary, {cloud_name: 'bitfoot'}), FileUploadModule],
@@ -73,7 +73,7 @@ describe('DepartmentView', () => {
 
   beforeEach(async(() => {
     activatedRoute.testData = {department: {id: 8, name: 'Dep1', handle: 'dep1', memberCategories: ['cat1', 'cat2']}};
-    fixture = TestBed.createComponent(DepartmentViewComponent);
+    fixture = TestBed.createComponent(DepartmentEditComponent);
     page = new Page();
     fixture.detectChanges();
     fixture.whenStable().then(() => {
