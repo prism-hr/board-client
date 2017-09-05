9fe6c5c9606161368492e4fef96b8d1532728ba2
Juan Mingo <juan.mingo@itv.com>
2017-09-05 17:55:43 +0200

    some ui changes

--- src/app/posts/apply/post-apply.dialog.ts
+++ src/app/posts/apply/post-apply.dialog.ts
@@ -11,47 +11,46 @@ import PostRepresentation = b.PostRepresentation;
     <h2 md-dialog-title>Apply for {{post.name}}</h2>
     <md-dialog-content>
       <form [formGroup]="eventForm" novalidate>
-        <div class="grid__item one-whole input-holder">
-          <b-file-upload formControlName="documentResume" type="document" class="uploader"></b-file-upload>
-          <control-messages [control]="eventForm.get('documentResume')"></control-messages>
-        </div>
-
-        <div class="grid__item one-whole input-holder">
-          <label>Your Resume URL</label>
-          <input pInputText type="url" formControlName="websiteResume">
-          <control-messages [control]="eventForm.get('websiteResume')"></control-messages>
-        </div>
-
-        <control-messages [control]="eventForm"></control-messages>
-
-        <div class="grid__item one-whole">
-          <label>Save Resume as default</label>
-          <p-checkbox formControlName="defaultResume" [binary]="true"></p-checkbox>
-        </div>
-
-        <div class="grid__item one-whole input-holder">
-          <label>Covering Note</label>
-          <p-editor formControlName="coveringNote" [style]="{'height':'150px'}">
-            <p-header>
+        <div class="grid">
+          <div class="grid__item one-whole input-holder">
+            <b-file-upload formControlName="documentResume" type="document" class="uploader"></b-file-upload>
+            <control-messages [control]="eventForm.get('documentResume')"></control-messages>
+          </div>
+          <div class="grid__item one-whole input-holder">
+            <label>Your Resume URL</label>
+            <input pInputText type="url" formControlName="websiteResume">
+            <control-messages [control]="eventForm.get('websiteResume')"></control-messages>
+          </div>
+          <control-messages [control]="eventForm"></control-messages>
+          <div class="grid__item one-whole input-holder">
+            <div class="ui-checkbox-inline">
+              <p-checkbox formControlName="defaultResume" [binary]="true" label="Save Resume as default"></p-checkbox>
+            </div>
+          </div>
+          <div class="grid__item one-whole input-holder">
+            <label>Covering Note</label>
+            <p-editor formControlName="coveringNote" [style]="{'height':'150px'}">
+              <p-header>
                 <span class="ql-formats">
                   <button class="ql-bold"></button>
                   <button class="ql-italic"></button>
                   <button class="ql-underline"></button>
                 </span>
-              <span class="ql-formats">
+                <span class="ql-formats">
                   <button class="ql-list" value="ordered" aria-label="Ordered List"></button>
                   <button class="ql-list" value="bullet" aria-label="Unordered List"></button>
                 </span>
-            </p-header>
-          </p-editor>
-          <control-messages [control]="eventForm.get('coveringNote')"></control-messages>
+              </p-header>
+            </p-editor>
+            <control-messages [control]="eventForm.get('coveringNote')"></control-messages>
+          </div>
         </div>
-
       </form>
     </md-dialog-content>
-    <md-dialog-actions>
-      <button pButton (click)="submit()" class="ui-button-success" label="Submit"></button>
+    <md-dialog-actions fxLayout="row" fxLayoutAlign="space-between center">
       <button pButton (click)="cancel()" class="ui-button-secondary" label="Cancel"></button>
+      
+      <button pButton (click)="submit()" class="ui-button-success" label="Submit"></button>
     </md-dialog-actions>
   `,
   styles: [`
