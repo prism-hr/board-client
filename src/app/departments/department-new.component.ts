0245417b01685979637b03588d6765f2729ea25c
Juan Mingo <juan.mingo@itv.com>
2017-09-05 13:04:59 +0200

    New ui updates

--- src/app/departments/department-new.component.ts
+++ src/app/departments/department-new.component.ts
@@ -3,16 +3,14 @@ import {Component} from '@angular/core';
 @Component({
   template: `
     <section class="section">
-      <md-card>
-        <md-card-header class="header-full">
-          <md-card-title>
-            <div>
-              <h2>Create New Department</h2>
-            </div>
-          </md-card-title>
-        </md-card-header>
-          <router-outlet></router-outlet>
-      </md-card>
+      
+     
+        <div class="section-header">
+          <h2>Create New Department</h2>
+        </div>
+      
+      <router-outlet></router-outlet>
+     
     </section>
   `
 })
