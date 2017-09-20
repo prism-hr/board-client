9034578e307d79639522da9b68be6f9c152daa14
fibinger <kuba@fibinger.pl>
2017-09-20 09:00:41 +0200

    Fixed scrolling problem when navigating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
 import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'b-app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <b-header></b-header>
+    <div class="page-width flex-grow">
+      <router-outlet></router-outlet>
+    </div>
+    <footer-component></footer-component>
+  `,
+  styleUrls: []
 })
 export class AppComponent implements OnInit {
 
-  constructor(private translate: TranslateService) {
+  constructor(private router: Router, private translate: TranslateService) {
     translate.use('en');
   }
 
   ngOnInit(): void {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
+    });
   }
-
 }
