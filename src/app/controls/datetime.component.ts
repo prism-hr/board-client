05e18a79ccabc18edc82c368067d857d7b690ed0
fibinger <kuba@fibinger.pl>
2017-11-07 19:41:40 +0100

    Removed moment timezone dependency

--- src/app/controls/datetime.component.ts
+++ src/app/controls/datetime.component.ts
@@ -1,42 +1,29 @@
 import {Component, ElementRef, forwardRef, OnInit} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import * as moment from 'moment';
-import 'moment-timezone';
 import {Utils} from '../services/utils';
 
 @Component({
   selector: 'b-datetime',
   template: `
-    <label>Specify date / time
-      <span *ngIf="currentTz !== mainTz">/ time zone</span>
-    </label>
+    <label>Specify date / time</label>
     <p-calendar class="calendar-item" [(ngModel)]="date" (ngModelChange)="dateTimeChanged()" dateFormat="yy-mm-dd"
-                dataType="string" [yearNavigator]="true" [monthNavigator]="true" [yearRange]="yearRange" 
+                dataType="string" [yearNavigator]="true" [monthNavigator]="true" [yearRange]="yearRange"
                 (onBlur)="touched()" [disabled]="isDisabled"></p-calendar>
     <p-inputMask class="time-item" [(ngModel)]="time" (ngModelChange)="dateTimeChanged()"
                  (onBlur)="touched()" [disabled]="isDisabled" mask="99:99"></p-inputMask>
-    <div *ngIf="currentTz !== mainTz" class="ui-radiobutton-inline time-zome-selector">
-      <p-radioButton [name]="timezoneRadioGroupName" [value]="mainTz" [label]="mainTz" [(ngModel)]="selectedTz"
-                     (ngModelChange)="dateTimeChanged($event)" [disabled]="isDisabled"></p-radioButton>
-      <p-radioButton [name]="timezoneRadioGroupName" [value]="currentTz" [label]="currentTz" [(ngModel)]="selectedTz"
-                     (ngModelChange)="dateTimeChanged($event)" [disabled]="isDisabled"></p-radioButton>
-    </div>
   `,
   styles: [`
     label {
       display: block
     }
 
-    .calendar-item, .time-item, .time-zome-selector {
+    .calendar-item, .time-item {
       display: inline-block;
     }
 
     .time-item {
       width: 64px
-    }
-
-    .time-zome-selector {
-      padding-left: 10px;
     }
   `],
   providers: [
@@ -49,32 +36,23 @@ import {Utils} from '../services/utils';
 })
 export class DateTimeComponent implements ControlValueAccessor, OnInit {
 
-  timezoneRadioGroupName: string;
   date: string;
   time: string;
   propagateChange: any;
   propagateTouch: any;
-  currentTz: string;
   selectedTz: string;
   mainTz = 'Europe/London';
   isDisabled: boolean;
   yearRange = Utils.getYearRange();
 
   constructor(public elementRef: ElementRef) {
-    const nameAttr = elementRef.nativeElement.getAttribute('name');
-    if (!nameAttr) {
-      throw new Error('Missing name attribute for b-datetime component');
-    }
-    this.timezoneRadioGroupName = nameAttr + 'Timezone';
   }
 
   ngOnInit(): void {
-    this.currentTz = (moment as any).tz.guess();
-    this.selectedTz = this.currentTz;
   }
 
   dateTimeChanged() {
-    const iso = (moment as any).tz(this.date + ' ' + this.time, this.selectedTz).tz(this.currentTz);
+    const iso = moment(this.date + ' ' + this.time);
     this.propagateChange(iso.isValid() ? iso.format().split('+')[0] : null);
   }
 
@@ -85,7 +63,7 @@ export class DateTimeComponent implements ControlValueAccessor, OnInit {
   writeValue(dateTime: string): void {
     if (dateTime) {
       const localDatetime = moment(dateTime);
-      const selectedDateTime = (localDatetime as any).tz(this.selectedTz).format();
+      const selectedDateTime = localDatetime.format();
       [this.date, this.time] = selectedDateTime.split('T');
       this.time = this.time.substr(0, 5);
     } else {
