8a65a0943ffdbd5ddb556ae63bd0c5a9fe3c7faf
fibinger <kuba@fibinger.pl>
2017-11-15 18:21:22 +0100

    More functionality for payments added

--- src/app/departments/department.service.ts
+++ src/app/departments/department.service.ts
@@ -5,6 +5,8 @@ import {Observable} from 'rxjs/Observable';
 import {EntityFilter} from '../general/filter/filter.component';
 import DepartmentRepresentation = b.DepartmentRepresentation;
 import UserRepresentation = b.UserRepresentation;
+import * as Stripe from 'stripe';
+import ICustomer = Stripe.customers.ICustomer;
 
 @Injectable()
 export class DepartmentService {
@@ -24,10 +26,20 @@ export class DepartmentService {
     return this.http.get('/api/departments/' + department.id + '/programs', {search: params}).map(res => res.json());
   }
 
-  putCustomer(department: DepartmentRepresentation, source: string) {
-    const params = new URLSearchParams();
-    params.set('source', source);
-    return this.http.put('/api/departments/' + department.id + '/customer?source=' + source, {}).map(res => res.json());
+  getPaymentSources(department: DepartmentRepresentation): Observable<ICustomer> {
+    return this.http.get('/api/departments/' + department.id + '/paymentSources').map(res => res.json());
+  }
+
+  postPaymentSource(department: DepartmentRepresentation, source: string) {
+    return this.http.post('/api/departments/' + department.id + '/paymentSources/' + source, {}).map(res => res.json());
+  }
+
+  deletePaymentSource(department: DepartmentRepresentation, source: string) {
+    return this.http.delete('/api/departments/' + department.id + '/paymentSources/' + source).map(res => res.json());
+  }
+
+  setPaymentSourceAsDefault(department: DepartmentRepresentation, source: string) {
+    return this.http.post('/api/departments/' + department.id + '/paymentSources/' + source + '/setDefault', {}).map(res => res.json());
   }
 
 }
--- src/app/departments/subscription/department-subscription.component.ts
+++ src/app/departments/subscription/department-subscription.component.ts
@@ -1,8 +1,11 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Data} from '@angular/router';
-import {Element as StripeElement, StripeService} from 'ngx-stripe';
-import DepartmentRepresentation = b.DepartmentRepresentation;
+import {ElementOptions, StripeCardComponent, StripeService} from 'ngx-stripe';
+import * as Stripe from 'stripe';
+import {ICard} from 'stripe';
 import {DepartmentService} from '../department.service';
+import DepartmentRepresentation = b.DepartmentRepresentation;
+import ICustomer = Stripe.customers.ICustomer;
 
 @Component({
   templateUrl: 'department-subscription.component.html',
@@ -10,8 +13,9 @@ import {DepartmentService} from '../department.service';
 })
 export class DepartmentSubscriptionComponent implements OnInit {
   department: DepartmentRepresentation;
-  card: StripeElement;
-  @ViewChild('card') cardRef: ElementRef;
+  customer: ICustomer;
+  elementOptions: ElementOptions;
+  @ViewChild(StripeCardComponent) card: StripeCardComponent;
 
   constructor(private route: ActivatedRoute, private stripeService: StripeService, private departmentService: DepartmentService) {
   }
@@ -20,43 +24,57 @@ export class DepartmentSubscriptionComponent implements OnInit {
     this.route.parent.parent.data
       .subscribe((parentData: Data) => {
         this.department = parentData['department'];
+        this.departmentService.getPaymentSources(this.department)
+          .subscribe(customer => {
+            this.customer = customer;
+          });
       });
 
-    this.stripeService.elements()
-      .subscribe(elements => {
-        // Only mount the element the first time
-        if (!this.card) {
-          this.card = elements.create('card', {
-            style: {
-              base: {
-                iconColor: '#666EE8',
-                color: '#31325F',
-                lineHeight: '40px',
-                fontWeight: 300,
-                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-                fontSize: '18px',
-                '::placeholder': {
-                  color: '#CFD7E0'
-                }
-              }
-            }
-          });
-          this.card.mount(this.cardRef.nativeElement);
+    this.elementOptions = {
+      style: {
+        base: {
+          iconColor: '#666EE8',
+          color: '#31325F',
+          lineHeight: '40px',
+          fontWeight: 300,
+          fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+          fontSize: '18px',
+          '::placeholder': {
+            color: '#CFD7E0'
+          }
         }
-      });
+      }
+    };
   }
 
   submitCard() {
     this.stripeService
-      .createToken(this.card, { name })
+      .createToken(this.card.getCard(), {name})
       .subscribe(result => {
+        this.card.getCard().clear();
         if (result.token) {
-          this.departmentService.putCustomer(this.department, result.token.id)
-            .subscribe();
+          this.departmentService.postPaymentSource(this.department, result.token.id)
+            .subscribe(customer => {
+              this.customer = customer;
+            });
         } else if (result.error) {
           // Error creating the token
           console.log(result.error.message);
         }
+      });
+  }
+
+  deleteSource(source: ICard) {
+    this.departmentService.deletePaymentSource(this.department, source.id)
+      .subscribe(customer => {
+        this.customer = customer;
+      });
+  }
+
+  setSourceAsDefault(source: ICard) {
+    this.departmentService.setPaymentSourceAsDefault(this.department, source.id)
+      .subscribe(customer => {
+        this.customer = customer;
       });
   }
 
