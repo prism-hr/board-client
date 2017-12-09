f53254089fd3648b890b2bf9248b8c2a6cf99d86
fibinger <kuba@fibinger.pl>
2017-12-09 13:39:35 +0100

    More headless configuration for UAT protractor

--- protractor-uat.conf.js
+++ protractor-uat.conf.js
@@ -1,6 +1,6 @@
 protractor = require('./protractor.conf.js');
 const config = protractor.config;
 
-config.capabilities.chromeOptions = {args: ["--headless"]};
+config.capabilities.chromeOptions = {args: ["--headless", 'no-sandbox', "--disable-gpu", "--window-size=800x600"]};
 
 exports.config = config;
