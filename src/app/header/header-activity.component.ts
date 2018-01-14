3a67cb05638a0ca85d814b7dde4df3bf94367d22
alastair <alastair.knowles@tide.co>
2018-01-14 19:48:13 +0000

    Added the missing activity cases

--- src/app/header/header-activity.component.ts
+++ src/app/header/header-activity.component.ts
@@ -103,6 +103,18 @@ import ResourceRepresentation = b.ResourceRepresentation;
               <span *ngSwitchCase="'RESTORE_POST_ACTIVITY'">
                 Your post <b>{{post.name}}</b> has been restored
               </span>
+              <span *ngSwitchCase="'CREATE_TASK_ACTIVITY'">
+                You have tasks for <b>{{department.name}}</b>
+              </span>
+              <span *ngSwitchCase="'UPDATE_TASK_ACTIVITY'">
+                You have tasks for <b>{{department.name}}</b>
+              </span>
+              <span *ngSwitchCase="'SUBSCRIBE_DEPARTMENT_ACTIVITY'">
+                Your trial period for <b>{{department.name}}</b> is coming to an end
+              </span>
+              <span *ngSwitchCase="'SUSPEND_DEPARTMENT_ACTIVITY'">
+                You missed a payment for <b>{{department.name}}</b>
+              </span>
               <p>
                 {{activity.createdTimestamp | date: 'short' }}
               </p>
