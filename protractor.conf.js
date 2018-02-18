b8caf72c82722d8ce8f65fccb87e1f51135d4d34
fibinger <kuba@fibinger.pl>
2018-02-18 15:15:13 +0100

    Updated Angular CLI version, tests fixed

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,6 +2,14 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+const screenshotReporter = new HtmlScreenshotReporter({
+  dest: 'target/screenshots',
+  filename: 'my-report.html',
+  captureOnlyFailedSpecs: true,
+  showQuickLinks: true
+});
 
 function randomString(length) {
   let text = '';
@@ -36,11 +44,22 @@ exports.config = {
   params: {
     randomString: randomString(4)
   },
+  beforeLaunch: function() {
+    return new Promise(function(resolve){
+      screenshotReporter.beforeLaunch(resolve);
+    });
+  },
   onPrepare() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(screenshotReporter);
+  },
+  afterLaunch: function(exitCode) {
+    return new Promise(function(resolve){
+      screenshotReporter.afterLaunch(resolve.bind(this, exitCode));
+    });
   },
   plugins: [{
     package: 'protractor-console',
