7c949a63f5197ff112a2f30122840575c3f4e0a1
fibinger <kuba@fibinger.pl>
2018-04-02 13:15:46 +0100

    Added global resource class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {NavigationEnd, Router} from '@angular/router';
+import {Component, HostBinding, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {TranslateService} from '@ngx-translate/core';
+import {BoardListComponent} from './boards/list/board-list.component';
 
 @Component({
   selector: 'b-app-root',
@@ -20,16 +21,42 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private router: Router, private translate: TranslateService) {
+  @HostBinding('class') resourceScope: string = null;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private translate: TranslateService) {
     translate.use('en');
   }
 
   ngOnInit(): void {
-    this.router.events.subscribe((evt) => {
-      if (!(evt instanceof NavigationEnd)) {
-        return;
-      }
-      window.scrollTo(0, 0)
-    });
+    this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .map(() => {
+        let child = this.activatedRoute.firstChild;
+        let resourceScope = null;
+        while (child) {
+          if (child.firstChild) {
+            child = child.firstChild;
+            const component = child.snapshot.component;
+            if (component && component['name'] === 'HomeComponent') {
+              return 'post';
+            } else if (component && component['name'] === 'BoardListComponent') {
+              return 'board';
+            } else if (component && component['name'] === 'DepartmentListComponent') {
+              return 'department';
+            }
+            if (child.snapshot.data && child.snapshot.data['resourceScope']) {
+              resourceScope = child.snapshot.data['resourceScope'];
+            }
+          } else {
+            return resourceScope;
+          }
+        }
+        return null;
+      })
+      .subscribe((resourceScope: string) => {
+        this.resourceScope = resourceScope || 'generic';
+        window.scrollTo(0, 0);
+      });
+
   }
 }
